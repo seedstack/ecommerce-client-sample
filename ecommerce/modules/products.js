010bcd612321c24c080491aae494ec3cbab209e8
kavi87 <kaviramyead@gmail.com>
2015-07-28 17:57:40 +0200

    refactor according to change in the catalog microservice

--- ecommerce/modules/products.js
+++ ecommerce/modules/products.js
@@ -14,15 +14,27 @@ define([
 
         var setup = function (resource) {
             $scope.resource = resource;
-            $scope.products = $scope.resource.$embedded('products');
-            $scope.next = $scope.resource.$links('next');
-            $scope.previous = $scope.resource.$links('prev');
+
+            $scope.products = resource.$embedded ? resource.$embedded('products') : [];
+
+            if (!($scope.products instanceof Array)) {
+                $scope.products = [$scope.products];
+            }
+
+            $scope.next = resource.$links('next');
+            $scope.previous = resource.$links('prev');
 
             angular.forEach($scope.products, function (product) {
-                 product.$links('tags').get(function (tags) {
-                     product.tags = tags.$embedded('tags');
-                });
+                if (product.$links) {
+
+                    product.$links('tags').get(function (tags) {
+                        product.tags = tags.$embedded('tags');
+                    });
+
+                }
+
             });
+
         };
 
         $scope.nextPage = function () {
@@ -35,13 +47,13 @@ define([
 
         $scope.search = function (query) {
             if (query) {
-                $scope.resource.$links('find', { q: query }).get(setup, error);
+                $scope.resource.$get({ q: query }, setup, error);
             } else {
                 hypermedia('ecommerce').enter('catalog', { page: 1 }).get(setup, error);
             }
         };
 
-        $scope.select = function (product) {
+        $scope.select = function (product, index) {
             product.$links('self').get(function (selectedProduct) {
                 dataService.selectedProduct(selectedProduct);
                 $location.path('/ecommerce/product/' + selectedProduct.name);
